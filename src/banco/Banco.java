46215ed9734061f91624e2f129bdb78e67b2e588
David Velasco <velascobladav@hotmail.com>
2020-03-22 20:25:34 +0100

    V2 Finalizada con debug

--- src/banco/Banco.java
+++ src/banco/Banco.java
@@ -13,11 +13,12 @@ import java.util.ArrayList;
 
 public class Banco {
     public static void main(String args[]){
+        System.out.println("Inicializando servidor...");    //DEBUG
         lecturaFichero();
-        guardarFichero();
         try{
             int serverPort = 8888; 
             ServerSocket listenSocket = new ServerSocket(serverPort);
+            System.out.println("Servidor en espera...");
             while(true) {
                 Socket clientSocket = listenSocket.accept();
                 Connection c = new Connection(clientSocket);
@@ -25,6 +26,7 @@ public class Banco {
         } catch(IOException e) {System.out.println("Listen socket:"+e.getMessage());}
     }
     public static boolean lecturaFichero(){
+        System.out.println("Leyendo fichero");    //DEBUG
         File fichero = new File("Cuentas.db");
 	Scanner s = null;
         try {
@@ -53,6 +55,7 @@ public class Banco {
         return true;
     }
     public static boolean guardarFichero(){
+        System.out.println("Guardando fichero");    //DEBUG
         FileWriter fichero = null;
         try {
             fichero = new FileWriter("Cuentas2.db");
@@ -69,10 +72,12 @@ public class Banco {
     }
     
     public static boolean addCuenta(int numeroCuenta,int saldo){
+        System.out.println("Añadiendo cuenta"+numeroCuenta+","+saldo);    //DEBUG
         Cuenta.Cuentas.add(new Cuenta(numeroCuenta, saldo));
         return true;
     }
     public static Cuenta buscarCuenta(int numeroCuenta){
+        System.out.println("Buscando cuenta"+numeroCuenta);    //DEBUG
         for (int i=0;i<Cuenta.Cuentas.size();i++){
             if(Cuenta.Cuentas.get(i).getNumeroCuenta()==numeroCuenta){
                 return Cuenta.Cuentas.get(i);
@@ -81,25 +86,35 @@ public class Banco {
         return null;
     }
     public static void procesarDato(DataInputStream in, DataOutputStream out) throws IOException{
+        System.out.println("Procesando dato");    //DEBUG
         String[] peticion;
-        ArrayList<String> respuesta=null;
+        ArrayList<String> respuesta=new ArrayList<>();
         int tarea;
-        String error="";
-        String respuestaUTF="";
-        //Menu en bucle, hasta recibir 0->Salida
+        String saldo,error,respuestaUTF;
+        //Menu en bucle, hasta recibir 5->Salida
         do{
+            //Inicializamos valores, cada vez que cambia la tarea
+            saldo="";
+            error="";               //Por defecto no hay error
+            respuesta.clear();
+            respuestaUTF="";
+
             peticion=in.readUTF().split(";");
             tarea=Integer.valueOf(peticion[0]);
+            System.out.println("Tarea "+tarea+",Cuenta"+peticion[1]);    //DEBUG
             Cuenta cCliente=buscarCuenta(Integer.valueOf(peticion[1]));
             if(cCliente!=null){ //Existe la cuenta
                 switch(tarea){
                     case 1: //lee_saldo
-                        respuesta.add(String.valueOf(cCliente.getSaldo()));
+                        System.out.println("Lectura saldo");    //DEBUG
+                        saldo=String.valueOf(cCliente.getSaldo());
                         break;
                     case 2: //Escribe saldo
+                        System.out.println("Escribe saldo");    //DEBUG
                         cCliente.setSaldo(Integer.valueOf(peticion[2]));
                         break;
                     case 3: //Bloquea_cuenta
+                        System.out.println("Bloquea");    //DEBUG
                         if(cCliente.isBloqueado()){
                             error="La cuenta esta bloqueada";
                         }else{
@@ -107,6 +122,7 @@ public class Banco {
                         }
                         break;
                     case 4: //Desbloquea Cuenta
+                        System.out.println("Desbloquea");    //DEBUG
                         if(cCliente.isBloqueado()){
                             cCliente.setBloqueado(false);
                         }else{
@@ -114,23 +130,30 @@ public class Banco {
                         }          
                         break;
                     case 5: //Termina servicio
+                        System.out.println("Termina");    //DEBUG
                         break;
                     default:
+                        System.out.println("Otra tarea");    //DEBUG
                         error="Operacion no valida";
                         break;
                 }                
             }else{
                 error="No existe la cuenta"; //No existe
-            }           
+            }        
+            System.out.println("ERROR:"+error);
+            if(error.equals("")){   //Si no hay error = 0
+                respuesta.add(0,"0");
+            }else{                  //Si hay error = 1
+                respuesta.add(0,"1");
+            }
+            if(tarea==1){
+                respuesta.add(saldo);
+            }
+            for(int i=0;i<respuesta.size();i++){
+                respuestaUTF+=respuesta.get(i)+";";
+            }
+            System.out.println(respuestaUTF);
+            out.writeUTF(respuestaUTF);
         }while(tarea!=5);
-        if(error.equals("")){   //Si no hay error = 0
-            respuesta.add("0");
-        }else{                  //Si hay error = 1
-            respuesta.add("1");
-        }
-        for(int i=0;i<respuesta.size();i++){
-            respuestaUTF+=";"+respuesta.get(i);
-        }
-        out.writeUTF(respuestaUTF);
     }
 }
